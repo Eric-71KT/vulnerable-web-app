0a21dfe4a551cf7480136d7975ae7609e2d003f2
Eric-71KT <canedoeric05@gmail.com>
2025-01-22 10:48:56 +0100

    Update UserInput.java

--- src/main/java/com/example/vulnerablewebapp/models/UserInput.java
+++ src/main/java/com/example/vulnerablewebapp/models/UserInput.java
@@ -1,12 +1,6 @@
 package com.example.vulnerablewebapp.models;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-
-@Controller
-public class UserInputController {
+public class UserInput {
     private String name;
 
     public String getName() {
@@ -15,16 +9,5 @@ public class UserInputController {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    @GetMapping("/displayUserInput")
-    public String displayUserInput(@RequestParam("name") String name, Model model) {
-        // Stockage direct de l'entrée utilisateur sans validation (vulnérabilité)
-        setName(name);
-
-        // Injection de l'entrée utilisateur dans le modèle, qui sera affiché dans une vue HTML
-        model.addAttribute("userInput", getName());
-
-        return "userInputView"; // Vue où la valeur sera affichée
     }
 }
